a9081e4bbce8c15d0fe72e6106ce48241759b587
Sebi <sebi_secara@yahoo.com>
2023-02-04 16:51:19 -0800

    BUILD: category & question routes

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -3,10 +3,37 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
+from sqlalchemy import func
 
 from models import setup_db, Question, Category
 
-QUESTIONS_PER_PAGE = 10
+RESULTS_PER_PAGE = 10
+
+def paginate_request(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * RESULTS_PER_PAGE
+    end = start + RESULTS_PER_PAGE
+
+    formatted_response = [question.format() for question in selection]
+    current_response = formatted_response[start:end]
+
+    return current_response
+
+def questions_query():
+    selection = Question.query.order_by(Question.id).all()
+    categories = Category.query.order_by(Category.id).all()
+    paginated_questions = paginate_request(request, selection)
+    formatted_categories = {category.id : category.type for category in categories}
+
+    if len(paginated_questions) == 0:
+        abort(404)
+    
+    return jsonify({
+        "success": True,
+        "questions": paginated_questions,
+        "total_questions": len(paginated_questions),
+        "categories": formatted_categories,
+    })
 
 def create_app(test_config=None):
     # create and configure the app
@@ -14,18 +41,45 @@ def create_app(test_config=None):
     setup_db(app)
 
     """
-    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs ✅
     """
+    CORS(app, resources={r"/categories": {"origins": "*"}})
 
     """
-    @TODO: Use the after_request decorator to set Access-Control-Allow
+    @TODO: Use the after_request decorator to set Access-Control-Allow ✅
     """
+    @app.after_request
+    def after_request(response):
+        # response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        # response.headers.add('Access-Control-Allow-Methods', 'GET')
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+
+        return response
 
     """
     @TODO:
     Create an endpoint to handle GET requests
     for all available categories.
     """
+
+    @app.route('/categories')
+    def get_categories():
+        selection = Category.query.order_by(Category.id).all()
+        categories = [category.format() for category in selection]
+
+        if len(categories) == 0:
+            abort(404)
+
+        return jsonify({
+            "success": True,
+            "categories": categories,
+            "total_categories": len(selection),
+        })
 
 
     """
@@ -41,6 +95,11 @@ def create_app(test_config=None):
     Clicking on the page numbers should update the questions.
     """
 
+    @app.route('/questions')
+    def get_questions():
+        questions = questions_query()
+
+        return questions
     """
     @TODO:
     Create an endpoint to DELETE question using a question ID.
@@ -48,6 +107,23 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
+
+    @app.route("/questions/<int:question_id>", methods=["DELETE"])
+    def delete_question(question_id):
+        try:
+            question = Question.query.filter(Question.id == question_id).one_or_none()
+
+            if question is None:
+                abort(404)
+            
+            question.delete()
+            questions = questions_query()
+
+            return questions
+
+
+        except:
+            abort(422)
 
     """
     @TODO:
@@ -97,6 +173,22 @@ def create_app(test_config=None):
     Create error handlers for all expected errors
     including 404 and 422.
     """
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": 'resource not found'
+        }), 404
+
+    @app.errorhandler(422)
+    def unporcessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": 'unporcessable'
+        }), 422
 
     return app
 
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -4,7 +4,7 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
-from models import setup_db, Question, Category
+from models import setup_db, Question, Category, db
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -14,14 +14,12 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        setup_db(self.app, self.database_path)
+        self.database_name = "trivia"
+        self.database_path = "postgres://{}/{}".format('student:student@localhost:5432', self.database_name)
 
         # binds the app to the current context
         with self.app.app_context():
-            self.db = SQLAlchemy()
-            self.db.init_app(self.app)
+            self.db = db
             # create all tables
             self.db.create_all()
     
@@ -33,6 +31,24 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
+    def test_get_paginated_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(len(data['categories']))
+        self.assertTrue(data['total_categories'])
+
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(len(data['categories']))
+        self.assertTrue(data['total_questions'])
 
 
 # Make the tests conveniently executable
